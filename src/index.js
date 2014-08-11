cc8341f81349dc087105c8924d2e37d35141c805
Mike Flynn <mfflynn@gmail.com>
2014-08-11 12:02:37 -0700

    master: Added button bar along the bottom to restore back/forward functionality.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 window.onresize = function(event) {
-  document.querySelector('#frame').width = window.innerWidth;
-  document.querySelector('#frame').width = window.innerHeight;
+  document.querySelector('#frame').style.width = window.innerWidth + "px";
+  document.querySelector('#frame').style.height = window.innerHeight - 36 + "px";
+};
+
+window.onload = function(event) {
+  document.querySelector('#backBtn').addEventListener("click", function(e) {
+    document.querySelector('#frame').back();
+  });
+
+  document.querySelector('#forwardBtn').addEventListener("click", function(e) {
+    document.querySelector('#frame').forward();
+  });
 };
